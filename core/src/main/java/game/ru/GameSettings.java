84686624012d634478156090eb1678f2e236b779
yigir10 <yigir111@gmail.com>
2026-04-23 18:57:13 +0300

    1

--- core/src/main/java/game/ru/GameSettings.java
+++ core/src/main/java/game/ru/GameSettings.java
@@ -1,4 +1,7 @@
 package game.ru;
 
 public class GameSettings {
+    public static final Object STEP_TIME = ;
+    public static final Object VELOCITY_ITERATIONS = ;
+    public static final Object POSITION_ITERATIONS = ;
 }
